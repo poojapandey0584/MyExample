23bbf370d55f94a56332fc6b6377545b2ddf36fc
Pandey, Ashok <ashok.pandey@conduent.com>
2019-08-09 09:43:57 -0400

    Removeunused Code
    Removeunused Code

---
+++ MyExample/CustomerDetails.aspx.cs
@@ -0,0 +1,60 @@
+﻿using Model.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyExample
+{
+    public partial class CustomerDetails : Page
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try {
+                GetCustomers();
+            } catch(Exception ex)
+            {
+
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        void GetCustomers()
+        {
+            try {
+
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:44333/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                   
+                    var response = client.GetAsync("api/customer").Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseString = response.Content.ReadAsStringAsync().Result;
+                        List<Customer> cust = JsonConvert.DeserializeObject<List<Customer>>(responseString);
+                        GridView1.DataSource = cust;
+                        GridView1.DataBind();
+                    }
+                }
+
+            } catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
