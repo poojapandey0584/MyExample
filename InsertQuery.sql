ad838ccc990ea52a6c324f1df80c46dc02390f6e
Pandey, Ashok <ashok.pandey@conduent.com>
2019-08-09 09:47:38 -0400

    added insert
    added insert

---
+++ InsertQuery.sql
@@ -0,0 +1,35 @@
+
+INSERT INTO [dbo].[customers]
+           ([Id]
+           ,[FName]
+           ,[LName]
+           ,[Address]
+           ,[Email]
+           ,[Telephone])
+     VALUES
+           (1
+           ,'Pooja'
+           ,'Pandey'
+           ,'245'
+           ,'pp@gmail.com'
+           ,'919-450-6777')
+GO
+
+
+INSERT INTO [dbo].[customers]
+           ([Id]
+           ,[FName]
+           ,[LName]
+           ,[Address]
+           ,[Email]
+           ,[Telephone])
+     VALUES
+           (1
+           ,'Pooja1'
+           ,'Pandey'
+            ,'245'
+           ,'pp@gmail.com'
+           ,'919-450-6777')
+GO
+
+
